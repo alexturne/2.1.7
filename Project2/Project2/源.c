d55b74bdcad74b5d7cf457ebdd82d775a7b13c8e
Alexturne <falaliku@icloud.com>
2018-07-06 11:57:27 +0800

    new

---
+++ Project2/Project2/源.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void smile(void);
+int main()
+{
+	smile();
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	getchar();
+	return 0;
+}
+void smile(void)
+{
+	printf("smile!");
+}
